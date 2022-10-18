f279138965193d4c7946fdc7850aa5f83310e1f5
Alex-Eriksen <31203252+Alex-Eriksen@users.noreply.github.com>
2022-10-18 19:14:01 +0200

    Added Networking of spells and mana consumption.

--- Wizard Battle Unity/Assets/Resources/Spells/Elemental Spells/Fire Spells/Fireball/FireballSpell.cs
+++ Wizard Battle Unity/Assets/Resources/Spells/Elemental Spells/Fire Spells/Fireball/FireballSpell.cs
@@ -6,19 +6,37 @@ using Mirror;
 public class FireballSpell : NetworkBehaviour
 {
     public ElementalSpellObject spellData = null;
-    public Vector2 movementDirection = Vector2.zero;
 
     [SerializeField] private float m_speed = 10f;
 
     private Rigidbody2D m_rigidbody2D;
+    private Transform m_transform;
 
     private void Awake()
     {
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_transform = transform;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DestroyDelay());
+    }
+
+    private IEnumerator DestroyDelay()
+    {
+        yield return new WaitForSeconds(spellData.LifeTime);
+        DestroySelf(gameObject);
+    }
+
+    [Command]
+    private void DestroySelf(GameObject obj)
+    {
+        NetworkServer.Destroy(obj);
     }
 
     private void FixedUpdate()
     {
-        m_rigidbody2D.velocity = m_speed * Time.deltaTime * movementDirection;
+        m_rigidbody2D.velocity = m_speed * Time.deltaTime * m_transform.up;
     }
 }
--- Wizard Battle Unity/Assets/Scripts/Player/PlayerCombat.cs
+++ Wizard Battle Unity/Assets/Scripts/Player/PlayerCombat.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Mirror;
+using UnityEditor.Experimental.GraphView;
 
 public class PlayerCombat : NetworkBehaviour
 {
     private PlayerInput m_playerInput;
     private PlayerEntity m_playerEntity;
-    private SpellCaster m_spellCaster;
     private bool m_isCasting;
+    private Vector2 m_mousePosition = Vector2.zero;
+    private Transform m_graphicsTransform;
+
+    private Spellbook m_spellbook;
 
     private void Awake()
     {
         m_playerInput = GetComponent<PlayerInput>();
         m_playerEntity = GetComponent<PlayerEntity>();
-        m_spellCaster = GetComponent<SpellCaster>();
+        m_graphicsTransform = transform.GetChild(1);
+        m_spellbook = FindObjectOfType<Spellbook>();
     }
 
     private void Start()
@@ -27,35 +32,62 @@ public class PlayerCombat : NetworkBehaviour
 
         m_playerInput.actions["LeftMouse"].started += LeftMouse_Started;
         m_playerInput.actions["RightMouse"].started += RightMouse_Started;
+        m_playerInput.actions["MousePosition"].performed += MousePosition_Performed;
+        m_playerInput.actions["Spellbook"].started += Spellbook_Started;
+        m_playerInput.actions["Spellbook"].canceled += Spellbook_Canceled;
     }
 
     private void RightMouse_Started(InputAction.CallbackContext obj)
     {
-        if(m_spellCaster.Spellbook.PrimarySelectedSpell == null || m_isCasting)
+        if (m_spellbook.SecondarySelectedSpell == null || m_isCasting)
         {
             return;
         }
 
-        m_isCasting = true;
-        m_playerEntity.CmdDrainMana(m_spellCaster.Spellbook.PrimarySelectedSpell.ManaCost);
-        CmdSpawnSpell(Resources.Load<GameObject>(m_spellCaster.Spellbook.PrimarySelectedSpell.PrefabPath));
+        SpawnSpell(m_spellbook.SecondarySelectedSpell);
     }
 
     private void LeftMouse_Started(InputAction.CallbackContext obj)
     {
-        if (m_spellCaster.Spellbook.SecondarySelectedSpell == null || m_isCasting)
+        if (m_spellbook.PrimarySelectedSpell == null || m_isCasting)
         {
             return;
         }
 
-        m_isCasting = true;
-        m_playerEntity.CmdDrainMana(m_spellCaster.Spellbook.SecondarySelectedSpell.ManaCost);
-        CmdSpawnSpell(Resources.Load<GameObject>(m_spellCaster.Spellbook.SecondarySelectedSpell.PrefabPath));
+        SpawnSpell(m_spellbook.PrimarySelectedSpell);
+    }
+
+    private void SpawnSpell(SpellObject spell)
+    {
+        //m_isCasting = true;
+        m_playerEntity.CmdDrainMana(spell.ManaCost);
+        CmdSpawnSpell(spell.PrefabPath);
     }
 
     [Command]
-    private void CmdSpawnSpell(GameObject spellPrefab)
+    private void CmdSpawnSpell(string spellPrefabPath)
     {
-        NetworkServer.Spawn(spellPrefab);
+        GameObject spawnedSpell = Instantiate(Resources.Load<GameObject>(spellPrefabPath));
+        spawnedSpell.transform.SetPositionAndRotation(m_graphicsTransform.position, m_graphicsTransform.rotation);
+        NetworkServer.Spawn(spawnedSpell, connectionToClient);
+    }
+
+    private void Spellbook_Started(InputAction.CallbackContext obj)
+    {
+        m_spellbook.OpenSpellbook();
+    }
+
+    private void Spellbook_Canceled(InputAction.CallbackContext obj)
+    {
+        m_spellbook.CloseSpellbook();
+    }
+
+    private void MousePosition_Performed(InputAction.CallbackContext obj)
+    {
+        m_mousePosition = obj.ReadValue<Vector2>();
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(m_mousePosition);
+        lookPos -= m_graphicsTransform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg - 90f;
+        m_graphicsTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
