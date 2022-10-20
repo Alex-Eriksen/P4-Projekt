7de730a5439b548ccff32b3b69afaa4bf96ae9ec
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2022-10-20 14:46:21 +0200

    Implemented basic chat system

---
+++ Wizard Battle Unity/Assets/Scripts/GUI/ChatBoxUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Mirror;
+
+public class ChatBoxUI : NetworkBehaviour
+{
+    [SerializeField] private Transform m_contentTransform;
+    [SerializeField] private GameObject m_messagePrefab;
+    [SerializeField] private TextMeshProUGUI m_messageText;
+
+    public void SendMessage()
+    {
+        CmdSendMessage("Player01", m_messageText.text);
+        m_messageText.text = "";
+    }
+
+    [Command(requiresAuthority = false)]
+    public void CmdSendMessage(string playerName, string messageText)
+    {
+        RpcRecieveMessage(playerName, messageText);
+    }
+
+    [ClientRpc]
+    public void RpcRecieveMessage(string playerName, string messageText)
+    {
+        ChatMessageUI obj = Instantiate(m_messagePrefab, m_contentTransform).GetComponent<ChatMessageUI>();
+        obj.Setup(playerName, messageText);
+    }
+}
