97d6d8ee03787083da482758d4f0f01dc8a3d5e3
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2022-10-24 13:51:23 +0200

    changed header & added chatting

--- Wizard Battle Web Client/src/app/components/fixed-components/chat/chat.component.ts
+++ Wizard Battle Web Client/src/app/components/fixed-components/chat/chat.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { JwtDecodePlus } from 'src/app/helpers/JWTDecodePlus';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { PlayerService } from 'src/app/services/player.service';
+import { DirectPlayerResponse } from 'src/app/_models/Player';
+import { PlayerChat } from './player';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'chat',
@@ -7,11 +13,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  @Output() openChat: EventEmitter<any> = new EventEmitter();
+
   chatOpen: boolean = true;
-  chatTab: boolean = true;
-  constructor() { }
+  chatTab: string = 'chat';
+  private playerId: number = 0;
+  playerName: string = "";
+  players: PlayerChat[] = []
+  constructor(private authenticationService: AuthenticationService, private playerService: PlayerService) { }
 
   ngOnInit(): void {
+    this.authenticationService.OnTokenChanged.subscribe(x =>
+      {
+        this.playerId = JwtDecodePlus.jwtDecode(x).nameid; // Gets playerId
+        this.playerService.getById(this.playerId).subscribe(data => this.playerName = data.playerName)
+      })
+
+      this.players.push({
+        playerName: "Alex",
+        playerSrc: "../../../../assets/alex.png",
+        playerStatus: "Away"
+      });
+
+      this.players.push({
+        playerName: "Nick",
+        playerSrc: "../../../../assets/alex.png",
+        playerStatus: "Online"
+      });
+
+      for(let i = 0; i < 50; i++) {
+        this.players.push({
+          playerName: `friend${i}`,
+          playerSrc: "../../../../assets/alex.png",
+          playerStatus: "Offline"
+        });
+    }
   }
 
+  addPlayer(playerName: string) {
+    console.log(playerName + " has been added to lobby");
+  }
+
+  toggleChat(newChatTab?: string) {
+    if(newChatTab == null) { // Close tab
+      this.chatOpen = false;
+      this.chatTab = '';
+      this.openChat.emit(null);
+    }
+    else {
+      if(newChatTab != null && !this.chatOpen) { // if new Chat is not null and chat is not open
+        this.openChat.emit(null);
+        this.chatTab = newChatTab!;
+        this.chatOpen = true;
+      }
+      else if(this.chatOpen && this.chatTab != newChatTab) {
+        this.chatTab = newChatTab!;
+      }
+    }
+  }
 }
