018fa9a677833c5cdffd001cb337856061c2d43a
MartinMidtgaard <martinmidtgaard@yahoo.com>
2022-10-31 08:13:54 +0100

    Only missing popup or buying items

--- Wizard Battle Web Client/src/app/app.module.ts
+++ Wizard Battle Web Client/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { AuthenticationInterceptor } from './_interceptor/authentication.interce
 import { appInitializer } from './helpers/app.initializer';
 import { AuthenticationService } from './services/authentication.service';
 import { ChatComponent } from './components/fixed-components/chat/chat.component';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ChatBoxComponent } from './components/fixed-components/chat-box/chat-box.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LibraryComponent } from './components/library/library.component';
@@ -31,16 +30,15 @@ import { LibraryComponent } from './components/library/library.component';
     SignupComponent,
     SpellbookComponent,
     ChatComponent,
-    ChatBoxComponent
-    ProfileComponent
+    ChatBoxComponent,
+    ProfileComponent,
     LibraryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ScrollingModule
+    FormsModule
   ],
   providers: [
     { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [ AuthenticationService ] },
