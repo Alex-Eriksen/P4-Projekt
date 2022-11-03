e365a0f2a10126fd2a231321f88481d0e0344bb1
MartinMidtgaard <martinmidtgaard@yahoo.com>
2022-11-03 14:27:41 +0100

    library menues

--- Wizard Battle Web Client/src/app/components/library/library.component.ts
+++ Wizard Battle Web Client/src/app/components/library/library.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Buymenu1Component } from '../modals/change-icon/buy-menues/buymenu1/buymenu1.component';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openBuyMenu(): void {
+    let dialogRef = this.dialog.open(Buymenu1Component, {
+      width: '536px',
+      maxWidth: '50vw',
+      height: '449px',
+
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log("Dialog has been closed");
+    });
+  }
 }
