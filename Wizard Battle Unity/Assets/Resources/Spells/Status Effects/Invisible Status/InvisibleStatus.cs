b513be3671d149f88bb1ce64d7af4f8811f75434
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2022-12-05 12:13:34 +0100

    Small Fix to Invisibility

--- Wizard Battle Unity/Assets/Resources/Spells/Status Effects/Invisible Status/InvisibleStatus.cs
+++ Wizard Battle Unity/Assets/Resources/Spells/Status Effects/Invisible Status/InvisibleStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class InvisibleStatus : Status
 {
@@ -8,6 +9,7 @@ public class InvisibleStatus : Status
     private CanvasGroup m_targetUI;
     private PlayerCombat m_targetPlayerCombat;
     private Color m_originalColor;
+    private ShadowCaster2D m_shadowCaster;
     [SerializeField] private Color m_targetColor, m_localTargetColor;
 
     protected override void OnSetup()
@@ -15,6 +17,7 @@ public class InvisibleStatus : Status
         m_targetSpriteRenderer = target.transform.Find("Graphics").GetComponent<SpriteRenderer>();
         m_targetUI = target.GetComponentInChildren<CanvasGroup>();
         m_targetPlayerCombat = target.GetComponent<PlayerCombat>();
+        m_shadowCaster = target.GetComponentInChildren<ShadowCaster2D>();
         m_originalColor = m_targetSpriteRenderer.color;
     }
 
@@ -38,17 +41,20 @@ public class InvisibleStatus : Status
     {
         m_targetSpriteRenderer.color = m_targetColor;
         m_targetUI.alpha = 0f;
+        m_shadowCaster.enabled = false;
     }
     private void EndInvisible()
     {
         m_targetSpriteRenderer.color = m_originalColor;
         m_targetUI.alpha = 1f;
+        m_shadowCaster.enabled = true;
     }
 
     private void LocalStartInvisible()
     {
         m_targetSpriteRenderer.color = m_localTargetColor;
         m_targetUI.alpha = 0.25f;
+        m_shadowCaster.enabled = false;
     }
     private void OnDestroy()
     {
